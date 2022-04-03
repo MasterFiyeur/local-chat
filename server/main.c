ea7a62dec3eb5fcc04ba09b6511d5700644dd590
MasterFiyeur <theo5julien@outlook.fr>
2022-04-03 19:18:32 +0200

    some comments

--- server/main.c
+++ server/main.c
@@ -1,6 +1,13 @@
 #include "../utils/lectureSecurisee.h"
 #include "user_management.h"
 
+/**
+*\brief This function used by a thread will manage TCP connection for write and read messages between clients
+    It should verify that the user is logged
+*
+*\param args [struct user *] shared memory of connected users with the request manager thread
+*\return void* Nothing
+ */
 void *communication(void* args){
     struct user *shared_memory = args;
     while (1)
@@ -17,6 +24,12 @@ void *communication(void* args){
     pthread_exit(NULL);
 }
 
+/**
+*\brief This function used by a thread will manage UDP connection for login/logout, creation/suppression of user, liste of connected users 
+*
+*\param args [struct user *] shared memory of connected users with the communication thread
+*\return void* Nothing
+ */
 void *request_manager(void* args){
     struct user *shared_memory = args;
     //Init username creation variables
@@ -42,11 +55,11 @@ void *request_manager(void* args){
 
 int main(int argc, char const *argv[])
 {
-    pthread_t com, req; //communication process and req process
-    struct user *shared_memory; //Shared memory variable
+    pthread_t com, req; //communication process and request manager process
+    struct user *shared_memory; //Shared memory variable (connected users)
     
-    //Shared memory init
-    shared_memory = mmap(NULL, MAX_USERS_CONNECTED*sizeof(struct user), (PROT_READ | PROT_WRITE), (MAP_SHARED | MAP_ANONYMOUS), -1, 0);
+    /* Shared memory of connected users initialization */
+    shared_memory = mmap(NULL, MAX_USERS_CONNECTED*sizeof(struct user), (PROT_READ | PROT_WRITE), (MAP_SHARED | MAP_ANONYMOUS), -1, 0); //Shared memory init
     //Init of users array to empty string as username and ip_add
     for (size_t i = 0; i < MAX_USERS_CONNECTED; i++){
         strcpy(shared_memory[i].username,"");
@@ -56,7 +69,7 @@ int main(int argc, char const *argv[])
     
     printf("Hello I'm the server !\n");
 
-    //Communication process creation
+    /* Communication thread creation */
     printf("Creation communication thread...");
     if(pthread_create( &com, NULL, communication, (void*)shared_memory))
         printf("\nError during thread creation\n");
@@ -65,18 +78,19 @@ int main(int argc, char const *argv[])
     //Create gap between 2 threads
     sleep(1);
 
-    //Request manager process creation
+    /* Request manager thread creation */
     printf("Creation request thread...");
     if(pthread_create( &req, NULL, request_manager, (void*)shared_memory)) //Thread creation
         printf("\nError during thread creation\n");
     printf("Created\n");
 
-    //Join communication and request manager threads
+    /* Join communication and request manager threads */
     pthread_join( com, NULL);
     pthread_join( req, NULL);
 
-    //Unmap shared_memory
+    /* Unmap shared_memory */
     munmap(shared_memory, MAX_USERS_CONNECTED*sizeof(char*));
+
     printf("Server finished.\n");
     return 0;
 }
