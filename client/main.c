27626d81f16bff124400ffe24d0eaa1c5fa51b8d
ZRunner <z.runner.mc@gmail.com>
2022-04-04 12:04:39 +0200

    add client signal handler

--- client/main.c
+++ client/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("Hello I'm the client !\n");
+#include "../utils/signals.h"
+
+static void handler(int sig, siginfo_t *info, void *ctx) {
+    printf("Received signal %s (%d) from PID: %d\n", get_signal_name(sig), sig, info->si_pid);
+    // close()
+}
+
+static void handle_signals(int signals[], int count) {
+    struct sigaction action;
+    memset(&action, '\0', sizeof(action));
+    action.sa_sigaction = &handler;
+
+    for (int i=0; i<count; i++) {
+        sigaction(signals[i], &action, NULL);
+    }
+}
+
+
+int main(int argc, char const *argv[]) {
+    // add signal handler for potentially-killing signals
+    int signals[6] = {SIGSTOP, SIGABRT, SIGINT, SIGQUIT, SIGTERM, SIGTSTP};
+    handle_signals(signals, sizeof(signals)/sizeof(signals[0]));
+
+    printf("Hello I'm the client with pid %d !\n", getpid());
+    sleep(10);
     return 0;
 }
